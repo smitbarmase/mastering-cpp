0a4ede584f071a0be225642ccc5b9c8a3c176790
Smit Barmase <smitbarmase@outlook.com>
2021-03-24 18:11:37 +0530

    iterators-to-values

--- 009-iterators/01-iterators-to-values.cpp
+++ 009-iterators/01-iterators-to-values.cpp
@@ -7,5 +7,10 @@ int main()
     auto my_iterator = my_vector.begin(); // position
     auto my_value = *my_iterator;         // value
     std::cout << my_value << std::endl;
+    ++my_iterator;
+    std::advance(my_iterator, 1);         // advance the iterator 1 position
+    my_iterator = std::next(my_iterator); // returns iterator to the next element
+    std::advance(my_iterator, -1);        // advance the iterator 1 position backwards
+    auto dist = std::distance(my_vector.begin(), my_iterator);
     return 0;
 }
