9216edae2c46f607ef72eb830c252d829cdec0ce
Smit Barmase <smitbarmase@outlook.com>
2021-03-24 20:03:37 +0530

    input output

---
+++ 010-input-output/01-input-output.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int value;
+    std::cout << "Enter a value: " << std::endl;
+    std::cin >> value;
+    std::cout << "The square of entered value is: " << value * value << std::endl;
+    return 0;
+}
