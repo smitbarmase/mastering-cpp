e1c46efe24df06736967797ff08ec7e79c3cca76
Smit Barmase <smitbarmase@outlook.com>
2021-03-24 17:19:23 +0530

    initialization

---
+++ 008-arrays/01-initialization.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int array_1[5];
+    int array_2[5] = {1, 2, 3, 4, 5};
+    int array_3[] = {1, 2, 3, 4, 5};
+    int array_4[5] = {1, 2}; // other will be 0s
+
+    return 0;
+}
